e17130ebff2faec1c742523346094fdf913355ca
Ark74 <ark@switnet.org>
2026-03-30 14:53:42 -0600

    [l10n]initial l10n files

---
+++ static/dashboard/public/lang/en.js
@@ -0,0 +1,10 @@
+window.i18n = {
+    "nav_home": "Home",
+    "nav_maps": "Maps",
+    "nav_kiwix": "Kiwix",
+    "footer_version": "IIAB-oA Dashboard v1.0",
+    "footer_about": "About IIAB-oA Dashboard",
+    "about_text": "This dashboard is designed to manage IIAB content on Android devices.",
+    "status_running": "Running",
+    "status_stopped": "Stopped"
+};
---
+++ static/dashboard/public/lang/es.js
@@ -0,0 +1,10 @@
+window.i18n = {
+    "nav_home": "Inicio",
+    "nav_maps": "Mapas",
+    "nav_kiwix": "Kiwix",
+    "footer_version": "IIAB-oA Dashboard v1.0",
+    "footer_about": "Acerca de IIAB-oA Dashboard",
+    "about_text": "Este panel está diseñado para gestionar contenido de IIAB en dispositivos Android.",
+    "status_running": "En ejecución",
+    "status_stopped": "Detenido"
+};
